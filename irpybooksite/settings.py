045dd2722fb062831762d66d2e8308ea1de97788
Rafael Silva <nightmarezlolzin@gmail.com>
2024-02-02 22:13:36 -0300

    Configuração inicial para o deploy

--- irpybooksite/settings.py
+++ irpybooksite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^(y27txw10vwj^=u(ldq9-q3v#*1@15)x808kj#%erh(wen&(e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'irpybooksite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '*Acdbf12B6CaC3AFcbD3GcbbfBFFGba*',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '20403',
     }
 }
 
@@ -119,6 +123,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') 
+
+
+
+
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
